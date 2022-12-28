48adb2a8c02ae024e6f00450425026edfd0d2200
arcas <andre.ruff@posteo.de>
2022-12-28 19:06:04 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # twitch_clip_BTTV_emote_generator
  Python Script that uses FFMPEG to generate BTTV ready emotes
+
+There's still some handywork required - mostly because this is a pretty useless, small script.
+
+## How to use:
+1. Download and install (Python)[https://www.python.org/downloads/windows/]; Linux users please use your package manager to do so; Mac Users stop using Mac.
+1. Download and install (ffmpeg)[https://ffmpeg.org/download.html]
+1. Run ´pip -r requirements.txt´ to install `yt-dlp`
+1. Open a CLI (Windows: PowerShell) and run `python bttv_emote_maker.py`
+1. Paste the URL of the Clip you want to use. You could technically also use complete VODs but that's going to generate a fuckton of garbo. Don't.
+1. When promoted to `Enter the start frame:` navigate to the Scrip Folder => cache. Scroll through the images to select your start enter the number (e.g. You want to start with clip_038.png enter 038. Keep the leading 0)
+1. When promted to `Enter the end frame:` do the same as above - just chose where your BTTV Emote should end
+1. Enter the desired file name ending in .gif (e.g. `kiviSkills.gif`)
+1. Open the Starting Frame in an image program. For ease of use I recommend something thats easily available such as (PhotoPea)[https://photopea.com]
+1. Use the selection tool and set it to a **fixed size** of 448×448px. Select the area you want to use and note the top coordinate and the left coordinate. (e.g. 557px from Top, 1382px from the left)
+1. Wait until the script is done. It will now crop the picutre, generate a GIF palette and create a 112×112px GIF that can be used on BTTV.
+
+## Disclaimer:
+This is mostly a little BS thing I did and offer as is. Feel free to fork and improve, I am done with it. :) 
---
+++ bttv_emote_maker.py
@@ -0,0 +1,47 @@
+import os
+import subprocess
+
+def download_clip(url, output_prefix):
+    # Create the "cache" subfolder if it doesn't exist
+    if not os.path.exists("cache"):
+        os.makedirs("cache")
+
+    # Use yt-dlp to download the clip
+    subprocess.run(["yt-dlp", url, "-o", f"cache/{output_prefix}.mp4"])
+
+    # Use ffmpeg to split the clip into individual frames
+    subprocess.run(["ffmpeg", "-i", f"cache/{output_prefix}.mp4", "-vf", "fps=10/1", f"cache/{output_prefix}_%03d.png"])
+   
+
+def create_gif(input_prefix, start_frame, end_frame, output_filename, top, left, width, height):
+    # Use ffmpeg to generate the GIF Area
+    subprocess.run(["ffmpeg", "-i", f"cache/{output_prefix}_%03d.png", "-vf", f"crop=448:448:{left}:{top}", f"cache/cropped_{output_prefix}_%03d.png"])
+    
+    # Generate the palette using ffmpeg
+    subprocess.run(["ffmpeg", "-y", "-f", "image2", "-start_number", str(start_frame), "-i", f"cache/cropped_{input_prefix}_%03d.png", "-vf", f"fps=10,scale={width}:{height}:flags=lanczos,palettegen=stats_mode=single", f"cache/palette_{input_prefix}_%03d.png"])
+
+    # Create the GIF using ffmpeg
+    subprocess.run(["ffmpeg", "-y", "-f", "image2", "-start_number", str(start_frame), "-i", f"cache/cropped_{input_prefix}_%03d.png", "-i", f"palette_{input_prefix}_%03d.png", "-lavfi", f"fps=10,scale=112:112:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle", output_filename])
+
+# Get the URL from the user
+url = input("Enter the URL of the clip: ")
+output_prefix = "clip"
+
+# Download the clip
+download_clip(url, output_prefix)
+
+# Get the start and end frames from the user
+start_frame = int(input("Enter the start frame: "))
+end_frame = int(input("Enter the end frame: "))
+output_filename = input("Enter the output filename: ")
+
+# Get the top and left coordinates of the area from the user
+top = int(input("Enter the top coordinate of the area: "))
+left = int(input("Enter the left coordinate of the area: "))
+
+# Set the width and height of the area to 448x448 pixels
+width = 448
+height = 448
+
+# Create the GIF
+create_gif(output_prefix, start_frame, end_frame, output_filename, top, left, width, height)
