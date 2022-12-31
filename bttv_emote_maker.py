e07c2412701d8d44c937511508e80bb18508c5bb
arcas <andre.ruff@posteo.de>
2022-12-31 02:58:11 +0100

    Reworked

--- bttv_emote_maker.py
+++ bttv_emote_maker.py
@@ -2,75 +2,44 @@ import os
 import subprocess
 import shutil
 
-def download_clip(url, output_prefix):
-    # Message to reinsure the user that everything is normal
-    print("Downloading the clip and splitting it in the individual frames")
-    
-    # Remove the "cache" folder and all of its contents
-    shutil.rmtree("cache", ignore_errors=True)
+# Create the cache folder if it doesn't exist
+shutil.rmtree("cache", ignore_errors=True)
+os.makedirs("cache")
 
-    # Recreate the "cache" folder
-    os.makedirs("cache", exist_ok=True)
-
-    # Use yt-dlp to download the clip
-    subprocess.run(["yt-dlp", url, "-o", f"cache/{output_prefix}.mp4"])
-
-    # Use ffmpeg to split the clip into individual frames
-    subprocess.run(["ffmpeg", "-loglevel", "quiet", "-i", f"cache/{output_prefix}.mp4", "-vf", "fps=10/1", f"cache/{output_prefix}_%03d.png"])
-   
-
-def create_gif(input_prefix, start_frame, end_frame, output_filename, top, left, width, height):
-    # Use ffmpeg to generate the GIF Area
-    subprocess.run(["ffmpeg","-loglevel", "quiet", "-i", f"cache/{output_prefix}_{start_frame:03d}.png", "-vf", f"crop=448:448:{left}:{top}", f"cache/cropped_{output_prefix}_{start_frame:03d}.png"])
-    
-    # Generate the palette using ffmpeg
-    subprocess.run(["ffmpeg","-loglevel", "quiet", "-y", "-f", "image2", "-start_number", str(start_frame), "-i", f"cache/cropped_{input_prefix}_{start_frame:03d}-{end_frame:03d}.png", "-vf", f"fps=10,scale={width}:{height}:flags=lanczos,palettegen=stats_mode=single", f"cache/palette_{input_prefix}_{start_frame:03d}.png"])
-
-    # Create the GIF using ffmpeg
-    subprocess.run(["ffmpeg","-loglevel", "quiet", "-y", "-f", "image2", "-start_number", str(start_frame), "-i", f"cache/cropped_{input_prefix}_{start_frame:03d}-{end_frame:03d}.png", "-i", f"cache/palette_{input_prefix}_{start_frame:03d}.png", "-lavfi", f"fps=10,scale=112:112:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle", output_filename])
-
-# Get the URL from the user
+# Get the URL of the video from the user
 url = input("Enter the URL of the clip: ")
-output_prefix = "clip"
 
-# Download the clip
-download_clip(url, output_prefix)
+# Download the video using yt-dlp
+subprocess.run(["yt-dlp","-q","--progress", "-o", "cache/video.mp4", url])
+
+print("Slicing clip into PNGs")
+
+# Split the video into frames with a frame rate of 10 fps
+subprocess.run(["ffmpeg", "-loglevel", "quiet", "-i", "cache/video.mp4", "-vf", "fps=10/1", "cache/clip_%03d.png"])
+
+print("Open Cache folder to pick frames")
 
 # Get the start and end frames from the user
 start_frame = int(input("Enter the start frame: "))
 end_frame = int(input("Enter the end frame: "))
-output_filename = input("Enter the output filename: ") or "bttv_emote.gif"
+filename = input("Enter the emote filename: ") or "new_bttv_emote.gif"
 
-# Get the top and left coordinates of the area from the user
-top = input("Enter the top coordinate of the area: ")
-left = input("Enter the left coordinate of the area: ")
+# Get the desired width, height, x, and y coordinates from the user
+x = int(input("Enter the top coordinate: "))
+y = int(input("Enter the left coordinate: "))
+width = int(input("Enter area width (default: 448): "))
+height = int(input("Enter area height (default: 448): "))
 
-# Get the width and height of the area from the user
-width = input("Enter the width of the area (default: 448): ")
-height = input("Enter the height of the area (default: 448): ")
+print("Generating emote...")
 
-# Set the default values if nothing was entered
-if top == "":
-    top = 0
-else:
-    top = int(top)
-if left == "":
-    left = 0
-else:
-    left = int(left)
-if width == "":
-    width = 448
-else:
-    width = int(width)
+index = 0
 
-if height == "":
-    height = 448
-else:
-    height = int(height)
+# Crop the frames using ffmpeg
+for i in range(start_frame, end_frame+1):
+    subprocess.run(["ffmpeg","-loglevel", "quiet", "-i", f"cache/clip_{i:03d}.png", "-filter:v", f"crop={width}:{height}:{x}:  {y}", "-frames:v", "1", f"cache/cropped_{index:03d}.png"])
+    subprocess.run(["ffmpeg", "-loglevel", "quiet", "-y", "-i", f"cache/cropped_{index:03d}.png", "-vf", "palettegen=stats_mode=single", f"cache/palette_{index:03d}.png"])
+    index += 1
 
+subprocess.run(["ffmpeg", "-loglevel", "quiet", "-i", f"cache/cropped_%03d.png", "-i", f"cache/palette_%03d.png", "-lavfi", "fps=10,scale=112:112:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle", filename])
 
-# Create the GIF
-create_gif(output_prefix, start_frame, end_frame, output_filename, top, left, width, height)
-
-# All done
-print(f"{output_filename} has been successfully generated")
+print(f"Emote {filename} successfully generated.")
