59d4373ffcd7bb3ce62e9b147d46bdc261400490
arcas <andre.ruff@posteo.de>
2022-12-31 13:40:41 +0100

    Update bttv_emote_maker.py
    Version 1 Release

--- bttv_emote_maker.py
+++ bttv_emote_maker.py
@@ -1,45 +1,125 @@
 import os
-import subprocess
 import shutil
+import subprocess
+import argparse
 
-# Create the cache folder if it doesn't exist
-shutil.rmtree("cache", ignore_errors=True)
-os.makedirs("cache")
+def download_clip(url, fps):
+    # Download the video using yt-dlp
+    print("Downloading the clip...")
+    subprocess.run(["yt-dlp","-q","--progress", "-o", f"{dir_path}/video.mp4", url])
 
-# Get the URL of the video from the user
-url = input("Enter the URL of the clip: ")
+    # Split the video into frames with a frame rate of 10 fps
+    print("Slicing clip into PNGs")
+    subprocess.run(["ffmpeg", "-loglevel", "quiet", "-i", f"{dir_path}/video.mp4", "-vf", f"fps={fps}/1", f"{dir_path}/clip_%03d.png"])
 
-# Download the video using yt-dlp
-subprocess.run(["yt-dlp","-q","--progress", "-o", "cache/video.mp4", url])
+    print(f"Open {dir_path} folder to pick frames")
+    user_input()
 
-print("Slicing clip into PNGs")
+def create_emote(start_frame, end_frame, filename, left, top, width, height, followLeft, followTop, size):
+    print("Cropping and generating palettes...")
+    index = 0
+    
+    # Crop the frames using ffmpeg
+    for i in range(start_frame, end_frame+1):
+        subprocess.run(["ffmpeg","-loglevel", "quiet", "-i", f"{dir_path}/clip_{i:03d}.png", "-filter:v", f"crop={width}:{height}:{left}:{top}", "-frames:v", "1", f"{dir_path}/cropped_{index:03d}.png"])
+        subprocess.run(["ffmpeg", "-loglevel", "quiet", "-y", "-i", f"{dir_path}/cropped_{index:03d}.png", "-vf", "palettegen=stats_mode=single", f"{dir_path}/palette_{index:03d}.png"])
+        index += 1
+        left += followLeft
+        top += followTop
 
-# Split the video into frames with a frame rate of 10 fps
-subprocess.run(["ffmpeg", "-loglevel", "quiet", "-i", "cache/video.mp4", "-vf", "fps=10/1", "cache/clip_%03d.png"])
+    # Generate the GIF by stitching everything together using the palette as index
+    print("Generating GIF...")
+    subprocess.run(["ffmpeg", "-loglevel", "quiet", "-i", f"{dir_path}/cropped_%03d.png", "-i", f"{dir_path}/palette_%03d.png", "-lavfi", f"fps=10,scale={size}:{size}:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle", filename])
 
-print("Open Cache folder to pick frames")
+    file_size = os.stat(filename).st_size
+    if file_size > 1048576:
+        print("The generated emote is over 1MB and will be to big for BTTV. Reduce the amout of frames.")
+        print(f"{filename} was generated")
+    else:
+        print(f"Emote {filename} successfully generated.")
 
-# Get the start and end frames from the user
-start_frame = int(input("Enter the start frame: "))
-end_frame = int(input("Enter the end frame: "))
-filename = input("Enter the emote filename: ") or "new_bttv_emote.gif"
+def complete_run():
+    # Get the URL of the video from the user
+    url = input("Enter the URL of the clip: ")
+    
+    download_clip(url, fps)
 
-# Get the desired width, height, x, and y coordinates from the user
-x = int(input("Enter the top coordinate: "))
-y = int(input("Enter the left coordinate: "))
-width = int(input("Enter area width (default: 448): "))
-height = int(input("Enter area height (default: 448): "))
+def user_input():# Get the specifications from user
+    start_frame = input("Enter the start frame: ") or 000
+    end_frame = input("Enter the end frame: ") or 100
+    filename = input("Enter the emote filename: ") or "new_bttv_emote.gif"
+    left = input("Enter the left coordinate: ") or 0
+    top = input("Enter the top coordinate: ") or 0
+    width = input("Enter area width (default: 448): ") or 448
+    height = input("Enter area height (default: 448): ") or 448
+    
+    # Adding default values just in case
+    start_frame = int(start_frame)
+    end_frame = int(end_frame)
+    left = int(left)
+    top = int(top)
+    width = int(width)
+    height = int(height)
 
-print("Generating emote...")
+    create_emote(start_frame, end_frame, filename, left, top, width, height, followLeft, followTop, size)
 
-index = 0
+# Generate arguments and help.
+parser = argparse.ArgumentParser()
+parser.add_argument("--regen", "-rg", action="store_true", help="Regenerate emote based on previous download.")
+parser.add_argument("--followleft", "-fl", type=int, help="Make left coordinate cropping follow a linear distance in pixel per frame.")
+parser.add_argument("--followtop", "-ft", type=int, help="Make left coordinate cropping follow a linear distance in pixel per frame.")
+parser.add_argument("--dir", "-d", type=str, help="Specify custom path. Useful if you want to keep previous files.")
+parser.add_argument("--fps", "-f", type=int, help="Slice with custom FPS. Standard is 10FPS")
+parser.add_argument("--outputsize", "-os", type=int, help="Change square output size. Useful if you want to make a GIF that you don't want to use on BTTV. 112x112px is standard.")
+parser.add_argument("--verbose", "-v", type=str, help="Don't clear the shell. (y/N)")
 
-# Crop the frames using ffmpeg
-for i in range(start_frame, end_frame+1):
-    subprocess.run(["ffmpeg","-loglevel", "quiet", "-i", f"cache/clip_{i:03d}.png", "-filter:v", f"crop={width}:{height}:{x}:  {y}", "-frames:v", "1", f"cache/cropped_{index:03d}.png"])
-    subprocess.run(["ffmpeg", "-loglevel", "quiet", "-y", "-i", f"cache/cropped_{index:03d}.png", "-vf", "palettegen=stats_mode=single", f"cache/palette_{index:03d}.png"])
-    index += 1
+args = parser.parse_args()
 
-subprocess.run(["ffmpeg", "-loglevel", "quiet", "-i", f"cache/cropped_%03d.png", "-i", f"cache/palette_%03d.png", "-lavfi", "fps=10,scale=112:112:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle", filename])
+# Argument Check: Cropping Path
+if args.followleft is not None:
+    followLeft = args.followleft
+else:
+    followLeft = 0
 
-print(f"Emote {filename} successfully generated.")
+if args.followtop is not None:
+    followTop = args.followleft
+else:
+    followTop = 0
+# Argument Check: Custom Path
+dir_path = args.dir or "cache"
+
+# Argument Check: Change FPS
+if args.fps is not None:
+    fps = args.fps
+else:
+    fps = 10
+
+# Argument Check: Change Output Size
+if args.outputsize is not None:
+    size = args.outputsize
+else:
+    size = 112
+    
+# Argument Check: Verbose
+if args.verbose == "y":
+    pass
+else:
+    os.system("clear" if os.name == "posix" else "cls")
+    
+# Argument Check: Make GIF using the previous clip
+if args.regen:
+    # Clean Up
+    for filename in os.listdir(dir_path):
+        # Check if the file name matches either of the specified formats
+        if filename.startswith("cropped_") and filename.endswith(".png") or filename.startswith("palette_") and filename.endswith(".png"):
+            os.remove(os.path.join(dir_path, filename))
+    user_input()
+else:
+    # Clean Up
+    if os.path.exists(dir_path):
+    # Delete the contents of the directory
+        for filename in os.listdir(dir_path):
+            os.remove(os.path.join(dir_path, filename))
+    else:
+        os.mkdir(dir_path)
+    complete_run()
